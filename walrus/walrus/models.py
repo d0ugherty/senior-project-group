25adee00eb7c8d22d7fdad2103cdc02de0270291
thomas <tdougherty16@gmail.com>
2023-10-18 13:44:56 -0400

    models.py

---
+++ walrus/walrus/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Projects(models.Model):
+    project_name = models.CharField(max_length=255)
+    date_created = models.DateTimeField()
+    is_complete = models.BooleanField()
+    date_completed = models.DateTimeField()
+    tasks = models.ManyToOneRel()
+
+class Task_Updates(models.Model):
+    Description = models.CharField(max_length=255)
+    Image = models.ImageField()
+
+class Employees(models.Model):
+    user = models.OneToOneField(User,
+    on_delete=models.CASCADE, null=True, blank=True)
+    Tasks = models.ManyToManyField(Tasks)
+    dept = models.CharField(max_length=255)
+    Employee_id = models.IntegerField()
+    Shifts = models.OneToManyField(Shift)
+    isManager = models.BooleanField()
+    Days_request_off = models.OneToManyField(Request_off)
+    Availability = OneToMany(avilable_time)
+
+class Tasks(models.Model):
+    task_name = models.CharField(max_length=255)
+    task_description = models.CharField(max_length=255)
+    is_complete = models.BooleanField()
+    Updates = models.OneToMany(Task_Updates)
+    date_created = models.DateTimeField()
+    Date_assigned_to = models.DateTimeField() # when the employee is supposed to start working on it
+    due_date = models.DateTimeField()
+    date_completed = models.DateTimeField()
+    Project_id = models.ForeignKey(Projects)
+    Assigned_emp = models.ManyToManyRel(User)
+    Time_spent_min = None
+    Time_spent_hour = None
+    if is_complete == True:
+        tot_sec = date_completed.total_seconds() - date_created.total_seconds()
+        Time_spent_min = tot_sec / 60
+        Time_spent_hour = Time_spent_min / 6
+
+
