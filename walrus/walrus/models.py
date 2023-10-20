73f6389665e00c20e51a83b9d6d1fc8ce7af9527
Mark O'Connor <oconno65@rowan.students.edu>
2023-10-20 14:18:10 -0400

    commented out some of the models so localhost could run

--- walrus/walrus/models.py
+++ walrus/walrus/models.py
@@ -6,11 +6,32 @@ class Projects(models.Model):
     date_created = models.DateTimeField()
     is_complete = models.BooleanField()
     date_completed = models.DateTimeField()
-    tasks = models.ManyToOneRel()
+   # tasks = models.ManyToOne()
 
 class Task_Updates(models.Model):
     Description = models.CharField(max_length=255)
-    Image = models.ImageField()
+   # Image = models.ImageField()
+
+
+
+class Tasks(models.Model):
+    task_name = models.CharField(max_length=255)
+    task_description = models.CharField(max_length=255)
+    is_complete = models.BooleanField()
+  #  Updates = models.OneToMany(Task_Updates)
+    date_created = models.DateTimeField()
+    Date_assigned_to = models.DateTimeField() # when the employee is supposed to start working on it
+    due_date = models.DateTimeField()
+    date_completed = models.DateTimeField()
+   # Project_id = models.ForeignKey(Projects)
+   # Assigned_emp = models.ManyToManyRel(User)
+    Time_spent_min = None
+    Time_spent_hour = None
+    if is_complete == True:
+        tot_sec = date_completed.total_seconds() - date_created.total_seconds()
+        Time_spent_min = tot_sec / 60
+        Time_spent_hour = Time_spent_min / 6
+
 
 class Employees(models.Model):
     user = models.OneToOneField(User,
@@ -22,23 +43,3 @@ class Employees(models.Model):
     isManager = models.BooleanField()
     #Days_request_off = models.OneToManyField(Request_off)
     #Availability = OneToMany(avilable_time)
-
-class Tasks(models.Model):
-    task_name = models.CharField(max_length=255)
-    task_description = models.CharField(max_length=255)
-    is_complete = models.BooleanField()
-    Updates = models.OneToMany(Task_Updates)
-    date_created = models.DateTimeField()
-    Date_assigned_to = models.DateTimeField() # when the employee is supposed to start working on it
-    due_date = models.DateTimeField()
-    date_completed = models.DateTimeField()
-    Project_id = models.ForeignKey(Projects)
-    Assigned_emp = models.ManyToManyRel(User)
-    Time_spent_min = None
-    Time_spent_hour = None
-    if is_complete == True:
-        tot_sec = date_completed.total_seconds() - date_created.total_seconds()
-        Time_spent_min = tot_sec / 60
-        Time_spent_hour = Time_spent_min / 6
-
-
