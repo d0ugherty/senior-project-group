128cb97c46ddee4e4864a2bf65cb26d725150458
Mark O'Connor <oconno65@rowan.students.edu>
2023-10-22 20:22:31 -0400

    renamed models.

--- walrus/walrus/views.py
+++ walrus/walrus/views.py
@@ -7,15 +7,16 @@ from .models import Task
 from .forms import taskSearchForm
 def list_tasks(request):
     try:
-        tasks = Tasks.objects.all()
+        tasks = Task.objects.all()
     except: 
         raise Http404('No Tasks Found')
+    
+
     if request.method == "POST":
         task_name = request.POST.get('task_name')
         project_name = request.POST.get('project_name')
         due_date = request.POST.get('due_date')
         status = request.POST.get('status')
-
 
        # print(task_name)
        # print(project_name)
