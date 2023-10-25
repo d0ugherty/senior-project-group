f58a286837584a67ddaea192bf1404f8e811c942
Kwak221 <113527506+Kwak221@users.noreply.github.com>
2023-10-25 12:39:11 -0400

    Added 'add_task' and 'delete_task'
    - DEV ONLY: tasks listed in calendar with delete method
    
    -add method only with name an description
    
    -only description and name listed

--- walrus/walrus/forms.py
+++ walrus/walrus/forms.py
@@ -18,3 +18,7 @@ class taskSearchForm(forms.Form):
     date = forms.DateField(label="Due Date", widget=DateInput, required=False)
     #Change Due_date to be a calander selection at some point
     status = forms.ChoiceField(choices=STATUS_CHOICES)
+
+class addTask(forms.Form):
+    task_name = forms.CharField(max_length=100)
+    description = forms.CharField(max_length=250)
--- walrus/walrus/views.py
+++ walrus/walrus/views.py
@@ -4,13 +4,14 @@ from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
 import datetime
-from datetime import datetime
+from datetime import datetime, timedelta
 from calendar import month_name
 from calendar import HTMLCalendar
 
 from .models import Task
-from .forms import taskSearchForm
+from .forms import taskSearchForm, addTask
 from .util import *
+
 def list_tasks(request):
    # try:
         #tasks = Task.objects.all()
@@ -50,6 +51,35 @@ def calendar(request):
     curMonth = datetime.now().month
     curYear = datetime.now().year
     curCalen =  HTMLCalendar().formatmonth(curYear, curMonth)
+    tasks = Task.objects.all()
     return render(request, 'calendar.html', {
         'curCalen': curCalen,
+        'tasks': tasks
     })
+
+def add_task(request):
+    if request.method=='POST':
+        form = addTask(request.POST)
+        if form.is_valid():
+            task_name = form.cleaned_data['task_name']
+            task_description = form.cleaned_data['description']
+            newTask = Task(
+                task_name = task_name,
+                task_description = task_description,
+            )
+
+            newTask.save()
+            return HttpResponseRedirect('/calendar')
+    else:
+        form = addTask()
+
+    return render(
+    request, 
+    'add_task.html',
+    {'form': form}
+    )
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return HttpResponseRedirect(reverse('calendar'))
