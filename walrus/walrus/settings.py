9b1e8809d3461d9b196308ea678c0213cea781ec
RLGagnon <113139718+RLGagnon@users.noreply.github.com>
2023-11-20 14:15:44 -0500

    Actual email backend email attempt. Pulled up older code. Not sure if all original edits are here, but this is what I could find.

--- walrus/walrus/settings.py
+++ walrus/walrus/settings.py
@@ -139,8 +139,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('User_Email')
+EMAIL_HOST_PASSWORD = os.environ.get('User_Password')
 
 
 # info so we know how to send to
