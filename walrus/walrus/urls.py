b15a9061e8817f5e4bc5eaba86d4faa7d3823ad1
Kwak221 <113527506+Kwak221@users.noreply.github.com>
2023-12-06 13:46:44 -0500

    Update urls.py

--- walrus/walrus/urls.py
+++ walrus/walrus/urls.py
@@ -20,7 +20,7 @@ from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
-from walrus import views
+from . import views
 
 from walrus.consumer import NotificationConsumer
 
@@ -30,7 +30,6 @@ urlpatterns = [
     path('task_detail/<task_id>', views.task_detail, name='task'),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", views.home_redirect, name="home"),
-    path('list_tasks/', views.list_tasks, name='list_tasks'),
     path('calendar/', views.CalendarView.as_view(), name='calendar'),
     path('add_task/', views.add_task, name='add_task'),
     path('delete_task/<int:task_id>', views.delete_task, name='delete_task'),
