40a49f7d76701a9fbfe1c5943ea7c0bf83f48e31
markoconnor9 <123832209+markoconnor9@users.noreply.github.com>
2023-12-10 16:18:26 -0500

    going to merge.

--- walrus/walrus/views.py
+++ walrus/walrus/views.py
@@ -239,7 +239,7 @@ def home_page(request, employee_id, day, month, year):
                 print(str(x) + " complete")
                 x.is_complete = True
                 x.save()
-                
+                return redirect('home')
         if "to_be_taken" in request.POST:
             shift_pk = request.POST['to_be_taken']
             shift = Shift.objects.get(pk=shift_pk)
